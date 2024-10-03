8e3a5a32c929fbc8475c9f7907debb32f7e73e56
semicolon213 <khk213624@gmail.com>
2024-10-03 17:35:31 +0900

    6주차 과제

---
+++ Week6_HW/Week6_HW/Resource.h
@@ -0,0 +1,59 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 다음에서 사용 Week6_HW.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_WEEK6HW_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_WEEK6HW			107
+#define IDI_SMALL				108
+#define IDC_WEEK6HW			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+
+//그리기 모드 선택에 대한 상수 정의입니다.
+#define MODE_STRAIGHT 5001
+#define MODE_RECT 5002
+#define MODE_ELLIPSE 5003
+#define MODE_FREE 5004
+
+//면색 선택에 대한 정의입니다.
+#define FACE_WHITE 6001
+#define FACE_BLACK 6002
+#define FACE_RED 6003
+#define FACE_BLUE 6004
+#define FACE_GREEN 6005
+
+//선색 선택에 대한 정의입니다.
+#define LINE_BLACK 7001
+#define LINE_RED 7002
+#define LINE_BLUE 7003
+#define LINE_GREEN 7004
+#define LINE_SKYBLUE 7005
+
+//색에 대한 정의입니다.
+#define BLACK RGB(0,0,0)
+#define WHITE RGB(255,255,255)
+#define RED RGB(255,0,0)
+#define BLUE RGB(0,0,255)
+#define GREEN RGB(0,255,0)
+#define SKYBLUE RGB(121,237,255)
+
+#endif
+// 다음은 새 개체에 사용할 기본값입니다.
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
